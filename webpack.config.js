81009117bc91b104e61301c65ae70aa86b7297d8
Nawarian <nickolas@phpsp.org.br>
2023-02-15 14:32:40 +0100

    feat(build) add copy webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,55 @@
 const path = require('path');
 const htmlPlugin = require('html-webpack-plugin');
+const copyPlugin = require('copy-webpack-plugin');
 
 const htmlPluginCfg = new htmlPlugin({
-  template: path.resolve(__dirname, 'index.html'),
-  filename: 'index.html',
-  inject: 'body',
+	template: path.resolve(__dirname, 'index.html'),
+	filename: 'index.html',
+	inject: 'body',
+});
+
+const copyPluginCfg = new copyPlugin({
+	patterns: [
+		{ from: 'assets/*.png', to: 'assets/[name].png' },
+		{ from: 'maps/*.json', to: 'maps/[name].json' },
+	],
 });
 
 module.exports = {
-  entry: './src/game.ts',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        include: path.resolve(__dirname, 'src'),
-        loader: 'ts-loader',
-      },
-      {
-        test: require.resolve('phaser'),
-        loader: 'expose-loader',
-        options: {
-          exposes: {
-            globalName: 'Phaser',
-            override: true,
-          },
-        },
-      },
-    ],
-  },
-  plugins: [htmlPluginCfg],
-  devServer: {
-    static: path.resolve(__dirname, './dist'),
-    host: 'localhost',
-    port: 8080,
-    open: false,
-    hot: true,
-    watchFiles: ['src/**/*.ts', 'index.html'],
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
+	entry: './src/game.ts',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				include: path.resolve(__dirname, 'src'),
+				loader: 'ts-loader',
+			},
+			{
+				test: require.resolve('phaser'),
+				loader: 'expose-loader',
+				options: {
+					exposes: {
+						globalName: 'Phaser',
+						override: true,
+					},
+				},
+			},
+		],
+	},
+	plugins: [htmlPluginCfg, copyPluginCfg],
+	devServer: {
+		static: path.resolve(__dirname, './dist'),
+		host: 'localhost',
+		port: 8080,
+		open: false,
+		hot: true,
+		watchFiles: ['src/**/*.ts', 'index.html'],
+	},
+	resolve: {
+		extensions: ['.ts', '.js'],
+	},
 };
